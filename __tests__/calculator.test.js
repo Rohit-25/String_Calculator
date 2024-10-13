e7118df91f51fc29b1a7e702e1a40b36326918ef
rohit <rohitgadekar6@gmail.com>
2024-10-13 12:29:06 +0530

    add function modified to handle different delimeters in input

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -19,3 +19,7 @@ test('should return 19 for "1,3,4,5,6" ', () => {
 test('should handle new lines between numbers', () => {
   expect(add("1\n2,3")).toBe(6);
 });
+
+test('should support different delimiters', () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
--- calculator.js
+++ calculator.js
@@ -1,7 +1,18 @@
 function add(numbers){
     if(!numbers) return 0;
+
+    let delimiter = /[\n,]/;
+
+    if(numbers.startsWith("//")){
+      const matchDelimeters = numbers.match(/\/\/(.)\n/);
+      // console.log(matchDelimeters)
+      delimiter = new RegExp(matchDelimeters[1]);
+      // console.log(numbers)
+      numbers= numbers.split("\n")[1]
+      console.log(numbers)
+    }
     
-    const numArray = numbers.split(/[\n,]/);
+    const numArray = numbers.split(delimiter);
     const ans = numArray.reduce((sum,num)=>sum + parseInt(num),0)
     return ans
     

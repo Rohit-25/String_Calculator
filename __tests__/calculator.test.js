037929c010ecf99eda88597868e9fcffccc0d0e1
rohit <rohitgadekar6@gmail.com>
2024-10-13 10:48:03 +0530

    added test case for comma-separated numbers as input

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -3,3 +3,11 @@ const add = require('../calculator.js')
 test('should return 0 for empty string input', () => {
   expect(add("")).toBe(0);
 });
+
+test('should return 1 for "1" input', () => {
+  expect(add("1")).toBe(1);
+});
+
+test('should return 6 for  "1" and "5" input', () => {
+  expect(add("1,5")).toBe(6);
+});

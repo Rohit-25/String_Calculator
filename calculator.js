9fcb9a96e19b82eda2477f8faa4a1fd3371d35f7
rohit <rohitgadekar6@gmail.com>
2024-10-13 11:54:38 +0530

    add function modified to handle new lines between numbers as input

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 function add(numbers){
     if(!numbers) return 0;
     
-    const numArray = numbers.split(',')
+    const numArray = numbers.split(/[\n,]/);
     const ans = numArray.reduce((sum,num)=>sum + parseInt(num),0)
     return ans
     

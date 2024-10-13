007df96d0d3fa87b554107fa99ee58dceaed9f4c
rohit <rohitgadekar6@gmail.com>
2024-10-13 10:56:48 +0530

    added test case to handle any amount of numbers

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -8,6 +8,10 @@ test('should return 1 for "1" input', () => {
   expect(add("1")).toBe(1);
 });
 
-test('should return 6 for  "1" and "5" input', () => {
+test('should return 6 for  "1,5" input', () => {
   expect(add("1,5")).toBe(6);
+});
+
+test('should return 19 for "1,3,4,5,6" ', () => {
+  expect(add("1,3,4,5,6")).toBe(19);
 });
